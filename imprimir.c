de48b4165ad36979535578d29dde69f84a17a7b7
Adriana-ku06 <adrianaku0609@gmail.com>
2020-05-10 10:04:20 -0500

    branch commit testing

--- imprimir.c
+++ imprimir.c
@@ -1,21 +1,20 @@
-/* Programa: Suma y multiplicación de dos números (Solución 1) */
+/* Programa: Suma y multiplicación de dos números (Solución 2) */
 
+#include <conio.h>
 #include <stdio.h>
 
 int main()
 {
-    int n1, n2, producto, suma;
+    int n1, n2;
 
-    printf( "\n   Introduzca primer numero (entero): " );
+    printf( "\n   Introduzca el primer numero (entero): " );
     scanf( "%d", &n1 );
-    printf( "\n   Introduzca segundo numero (entero): " );
+
+    printf( "\n   Introduzca el segundo numero (entero): " );
     scanf( "%d", &n2 );
 
-    suma = n1 + n2;
-    producto = n1 * n2;
-
-    printf( "\n   La suma es: %d", suma );
-    printf( "\n\n   La multiplicaci%cn es: %d", 162, producto );
+    printf( "\n   La suma es: %d", n1 + n2 );
+    printf( "\n\n   La multiplicaci%cn es: %d", 162, n1 * n2 );
 
     getch(); /* Pausa */
 
